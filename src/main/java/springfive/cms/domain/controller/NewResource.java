1f15da6011e2cbf121c62c187e611425da7b3304
eadogan <e.ahmetdogan@gmail.com>
2019-02-27 22:23:05 +0000

    Refactor NewsResource

--- src/main/java/springfive/cms/domain/controller/NewResource.java
+++ src/main/java/springfive/cms/domain/controller/NewResource.java
@@ -1,12 +1,52 @@
 package springfive.cms.domain.controller;
 
-public class NewResource {
-    private static NewResource ourInstance = new NewResource();
 
-    public static NewResource getInstance() {
-        return ourInstance;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import springfive.cms.domain.models.News;
+import springfive.cms.domain.models.Review;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/news")
+public class NewResource {
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<News> findOne(@PathVariable("id") String id){
+        return ResponseEntity.ok(new News());
     }
 
-    private NewResource() {
+    @GetMapping
+    public ResponseEntity<List<News>> findAll(){
+        return ResponseEntity.ok(Collections.singletonList(new News()));
+    }
+
+    @PostMapping
+    public ResponseEntity<News> saveNews(@RequestBody News news){
+        return new ResponseEntity<>(new News(), HttpStatus.CREATED);
+    }
+
+    @PatchMapping(value = "/{id}")
+    public ResponseEntity<News> updateNews(@PathVariable("id")String id, @RequestBody News news){
+        return new ResponseEntity<>(new News(), HttpStatus.OK);
+    }
+
+    @PatchMapping(value = "/{id}")
+    public ResponseEntity<News> deleteNews(@PathVariable("id")String id){
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+
+    @GetMapping(value = "/{id}/review/{userId}")
+    public ResponseEntity<Review> review(@PathVariable("id") String id, @PathVariable("userId")String userId){
+        return ResponseEntity.ok(new Review());
+    }
+
+    @GetMapping(value = "/revised")
+    public ResponseEntity<List<News>> revisedNews(){
+        return ResponseEntity.ok(Collections.singletonList(new News()));
     }
 }
